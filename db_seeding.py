3c8c91ad6f600c4baaea4926fddd061c6076ae1e
Franklin Stennett <franklinstennett@gmail.com>
2023-08-14 09:49:15 -0600

    added db_seeding

--- db_seeding.py
+++ db_seeding.py
@@ -1,11 +1,8 @@
 # Import libraries
+import sys
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chat_models import ChatOpenAI
-import os
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
 from langchain.document_loaders import DirectoryLoader
 
 def load_documents(path):
@@ -16,24 +13,25 @@ def split_text(doc):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     return text_splitter.split_documents(doc)
 
-def setup_db(docs, embedding):
-    
+def setup_db(docs, embedding, dir):
     return Chroma.from_documents(documents=docs,
                                      embedding=embedding,
-                                     persist_directory=persist_directory)
+                                     persist_directory=dir)
 
 if __name__ == '__main__':
 
-    persist_directory = 'db'
+    if len(sys.argv) == 1 or len(sys.argv) == 2:
+        raise Exception('db or document_path param missing. Usage: db_seeding.py "db folder name" "location of source documents"')
+
+    db_param = sys.argv[1]
+    document_path_param = sys.argv[2]
 
     embedding = OpenAIEmbeddings()
 
-    documents = load_documents('data/optimus')
+    documents = load_documents(document_path_param)
     splitted_docs = split_text(documents)
 
-    db = setup_db(splitted_docs, embedding)
+    db = setup_db(splitted_docs, embedding, db_param)
+    db.persist()
 
-    
-
-    
-
+    print('Database created.')
